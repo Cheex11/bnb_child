3ab2179719e779c5c322c5024d89a9ca99c9b9ba
Lauren Yeiser & Cody Wilson <student@epicodus.com>
2014-04-16 13:56:04 -0700

    update and destroy added to listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,48 @@
 class ListingsController < ApplicationController
+  def new
+    @listing = Listing.new
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+   if @listing.save
+      flash[:notice] = "Listing Added"
+      redirect_to listing_path(@listing)
+    else
+      flash[:notice] = "Fail"
+      render "new"
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    if @listing.update(listing_params)
+      flash[:notice] = "Listing Updated"
+      redirect_to :back
+    else
+      flash[:notice] = "Fail"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to user_path(current_user)
+  end
+
+private
+
+  def listing_params
+    params.require(:listing).permit(:price, :title, :location, :bedrooms, :bathrooms, :description, :user_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
     if !session[:user_id].nil?
     @user = User.find(params[:id])
    end
